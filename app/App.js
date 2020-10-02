e20ce8a5e98220f37912e0aa6fb5b002aecd21e4
Mahdi Nicoo <MahdiNicoo@gmail.com>
2020-10-03 03:13:22 +0330

    mv

--- app/App.js
+++ app/App.js
@@ -4,6 +4,9 @@ const Server = require('./server/Server')
 const Side = require('./side/Side')
 const Web = require('./web/Web')
 
+const HTTP = require('http')
+const cors = require('@koa/cors')
+
 const Koa = require('koa')
 const Router = require('@koa/router')
 const Static = require('./server/helpers/KoaStatic')
@@ -15,7 +18,37 @@ module.exports = async function App(config) {
 
     const data = await Data(config.data, side)
     
-    const server = await Server(data, config.server, config.controllers)
+    const {wsServers, middlewares} = await Server(data, config.server, config.controllers)
+
+    const router = new Router()
+    router.get(
+        '/storage/:folderName/:fileName',
+        Static(path.dirname(data.filer.getStorageDir()), {
+            maxage: 1000 * 60 * 60 * 24 * 90,
+        }),
+    )
+
+    const koa = new Koa()
+    koa.use(cors())
+    koa.use(router.routes())
+    koa.use(router.allowedMethods())
+    middlewares.forEach(middle => koa.use(middle))
+
+    const httpServer = HTTP.createServer(koa.callback())
+    await new Promise(resolve => httpServer.listen(config.port, e => e ? reject(error) : resolve()))
+    console.log(`🚀 Api Server ready at http://localhost:${config.port}`)
+
+    // https://github.com/apollographql/subscriptions-transport-ws/issues/199
+    httpServer.on('upgrade', (request, socket, head) => {
+        const pathname = url.parse(request.url).pathname
+        const name = pathname.substr(1, pathname.length - 1 - '/graphql'.length)
+        const wsServer = wsServers[name]
+        if (!wsServer) return socket.destroy()
+        
+        wsServer.wsServer.handleUpgrade(request, socket, head, function done(ws) {
+            wsServer.wsServer.emit('connection', ws, request)
+        })
+    })
 
     const web = Web(config.web)
 
--- app/server/Server.js
+++ app/server/Server.js
@@ -10,15 +10,7 @@ const Structure = require('./structure/Structure')
 const structureToSchema = require('./helpers/structureToSchema')
 const { SubscriptionServer } = require('subscriptions-transport-ws')
 const {execute, subscribe} = require('graphql')
-
-const path = require('path')
 const url = require('url')
-
-const HTTP = require('http')
-const Koa  = require('koa')
-const cors = require('@koa/cors')
-const Router = require('@koa/router')
-const Static = require('./helpers/KoaStatic')
 
 const ASK  = require('apollo-server-koa')
 
@@ -67,34 +59,6 @@ module.exports = async function Server(data, config, controllers) {
         wsServers[name] = SubscriptionServer.create({execute, subscribe, schema}, {noServer: true})
     }
 
-    const router = new Router()
-    router.get(
-        '/storage/:folderName/:fileName',
-        Static(path.dirname(data.filer.getStorageDir()), {
-            maxage: 1000 * 60 * 60 * 24 * 90,
-        }),
-    )
-
-    const koa = new Koa()
-    koa.use(cors())
-    koa.use(router.routes())
-    koa.use(router.allowedMethods())
-    middlewares.forEach(middle => koa.use(middle))
-
-    const httpServer = HTTP.createServer(koa.callback())
-    await new Promise(resolve => httpServer.listen(config.port, e => e ? reject(error) : resolve()))
-    console.log(`🚀 Api Server ready at http://localhost:${config.port}`)
-
-    // https://github.com/apollographql/subscriptions-transport-ws/issues/199
-    httpServer.on('upgrade', (request, socket, head) => {
-        const pathname = url.parse(request.url).pathname
-        const name = pathname.substr(1, pathname.length - 1 - '/graphql'.length)
-        const wsServer = wsServers[name]
-        if (!wsServer) return socket.destroy()
-        
-        wsServer.wsServer.handleUpgrade(request, socket, head, function done(ws) {
-            wsServer.wsServer.emit('connection', ws, request)
-        })
-    })
+    return {wsServers, middlewares}
     
 }
